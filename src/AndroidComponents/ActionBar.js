76e456156c1bd09c6da711319d709cead24b6a92
GalZilca <galzilca@gmail.com>
2020-09-26 13:15:22 +0300

    create react app

---
+++ src/AndroidComponents/ActionBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const ActionBar = () => (
+    <div className="header">
+        <p className="header__title">
+            title
+        </p>
+        <button>
+            <img></img>
+        </button>
+    </div>    
+)
